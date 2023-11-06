8adbbec34ef7269fe25828501ccc59a557617c11
Shah Limon <limon.working@gmail.com>
2023-11-06 15:46:09 +0600

    update

---
+++ src/Pages/Admin/EditCategory.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+const EditCategory = () => {
+  const [category, setCategory] = useState([]);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    fetch(`https://nft-seo-server.onrender.com/category/${id}`)
+      .then((res) => res.json())
+      .then((info) => setCategory(info));
+  }, [id]);
+
+  const handleEditCategory = (event) => {
+    event.preventDefault();
+    const Category = event.target.Category.value;
+    const editCategory = { Category };
+
+    const url = `https://nft-seo-server.onrender.com/edit-category/${category._id}`;
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(editCategory),
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        navigate(`/admin/categories`);
+      });
+  };
+
+  return (
+    <div>
+      <form className="new-category" onSubmit={handleEditCategory}>
+        <input
+          type="text"
+          defaultValue={category.Category}
+          placeholder="New category name"
+          name="Category"
+        ></input>
+        <input type="submit" className="btn ml-5" value="Edit Now"></input>
+      </form>
+    </div>
+  );
+};
+
+export default EditCategory;
---
+++ src/Pages/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Banner from "../components/Banner";
+
+
+const Home = () => {
+  return (
+    <div>
+     Home content
+     
+    </div>
+  );
+};
+
+export default Home;
